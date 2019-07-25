fd10e60428c4d38d169079b584040d0c00a05f33
Vio3301 <51275082+Vio3301@users.noreply.github.com>
2019-07-25 19:56:06 +0300

    Add files via upload

---
+++ JS2/fule/DINAMIC.js
@@ -0,0 +1,41 @@
+/* 
+function giveMoney(){
+    alert(1000)
+}
+giveMoney()
+giveMoney()
+giveMoney
+*/
+/*function calculateFuel(distan){
+       var consume = 8
+      return distan * consume /100
+}
+
+alert( calculateFuel(1000) );
+*/
+///
+
+
+
+///Calculeaza 
+
+
+/*
+function calculateConsum(distance, type){
+   // type = promp("Type")
+    switch(type){
+        case "bike": consume = 2; break;
+        case "car": consume = 8; break;
+        case "bus": consume = 10; break;
+
+    }
+    return consume * distance / 100
+}
+alert( calculateConsum(1000,"bike") );
+*/
+///DINAMIC PAGE///
+
+function show(text){
+    document.write( text )
+}
+show("Test OK!")
